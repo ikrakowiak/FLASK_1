f6bd1d2b1b9f4ee15294601501a48ca1684b1f21
Igor Krakowiak <ikrakowiak@st.swps.edu.pl>
2025-12-16 20:06:12 +0100

    Trening z grupą

--- 1_webpage/index.py
+++ 1_webpage/index.py
@@ -2,6 +2,9 @@ from flask import Flask, redirect, request, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+     return render_template("base.html", title="Base")
 
 if __name__ == "__main__":
     app.run(debug=True, port=3333)
